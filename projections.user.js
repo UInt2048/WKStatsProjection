2493a77a0e32fa1652fa8d6e0200b8d93848146c
UInt2048 <46784000+UInt2048@users.noreply.github.com>
2022-01-18 21:02:35 -0500

    Tidy codebase

--- projections.user.js
+++ projections.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WKStats Projections Page
-// @version      1.1.4
+// @version      1.1.5
 // @description  Make a temporary projections page for WKStats
 // @author       UInt2048
 // @include      https://www.wkstats.com/*
@@ -10,34 +10,51 @@
 // ==/UserScript==
 
 //jshint esversion:6
+/*eslint max-len: ["error", { "code": 120 }]*/
 
 (function() {
     'use strict';
     window.wkof.include('ItemData, Apiv2');
 
-    var maxLevel = null, progressions = [], current = null, stats = null;
+    let maxLevel = null, progressions = [], current = null, stats = null;
 
-    function addGlobalStyle(css) {
-        var head, style;
-        head = document.getElementsByTagName('head')[0];
-        if (!head) { return; }
-        style = document.createElement('style');
-        style.type = 'text/css';
-        style.innerHTML = css.replace(/;/g, ' !important;');
-        head.appendChild(style);
+    const addGlobalStyle = function addGlobalStyle() {
+        const head = document.getElementsByTagName('head')[0], css = `
+.main-content .chart table.coverage {margin-top:1em; position:relative;}
+.main-content .chart table.coverage {border-collapse:collapse; border-spacing:0; margin-left:auto; margin-right:auto;}
+.main-content .chart table.coverage tr {border-left:1px solid #000;}
+.main-content .chart table.coverage tr:first-child {border-top:1px solid #000;}
+.main-content .chart table.coverage tr:last-child {border-bottom:1px solid #000;}
+.main-content .chart table.coverage tr.header {background-color:#ffd; font-weight:bold;}
+.main-content .chart table.coverage tr.header:nth-child(2) {line-height:1em;}
+.main-content .chart table.coverage tr.header.bottom {border-bottom:1px solid #000;}
+.main-content .chart table.coverage tr:not(.header) + tr:not(.header):not(.current_level) {border-top:1px solid #ddd;}
+.main-content .chart table.coverage tr:not(.header):nth-child(even) {background-color:#efe;}
+.main-content .chart table.coverage td {padding:0 .5em;}
+.main-content .chart table.coverage td:first-child {border-right:1px solid #000;}
+.main-content .chart table.coverage td:last-child {border-right:1px solid #000;}
+.main-content .chart table.coverage tr.header td.header_div {border-bottom:1px solid #0001;}
+.main-content .chart table.coverage tr.count td {font-weight:normal; font-size:0.625em;}
+.main-content .chart table.coverage tr.current_level {border:2px solid #000;}
+.main-content .chart table.coverage tr.current_level:after
+{content:"\f061"; font-family:FontAwesome; position:absolute; display:inline-block; left:-1.25em;}
+        `;
+        if (head) {
+            const style = document.createElement('style');
+            style.type = 'text/css';
+            style.innerHTML = css.replace(/;/g, ' !important;');
+            head.appendChild(style);
+        }
     }
 
-    window.setHidePast = () => {
-        history.pushState({}, null, document.URL.includes('#hidePast') ? '#' : '#hidePast');
-        window.project(null);
+    const set = function set(url) {
+        return () => {
+            history.pushState({}, null, url);
+            project();
+        };
     }
 
-    window.setCurrent = () => {
-        history.pushState({}, null, '#L' + current.level);
-        window.project(null);
-    }
-
-    window.project = speed => {
+    const project = function project() {
         Date.prototype.add = function(seconds) {
             this.setTime(this.getTime() + (seconds*1000));
             return this;
@@ -51,30 +68,32 @@
                 return new Intl.DateTimeFormat([], { dateStyle: 'medium', timeStyle: 'medium' }).format(this);
             } catch (e) {
                 if (e instanceof RangeError) {
-                    return Infinity;
-                } else throw e;
+                    console.log("Project called too soon!");
+                }
+                throw e;
             }
         }
-        function median(arr) {
+        const median = function median(arr) {
             const mid = Math.floor(arr.length / 2);
             return arr.length % 2 !== 0 ? arr[mid] : (arr[mid - 1] + arr[mid]) / 2;
         }
 
-        var levels = progressions.map($0 => $0.level);
+        const levels = progressions.map($0 => $0.level);
 
-        var levelDuration = (level => new Date(level.passed_at ? level.passed_at : level.abandoned_at).subtractDate(new Date(level.unlocked_at)));
+        const levelDuration = (level => new Date(level.passed_at ? level.passed_at : level.abandoned_at).
+                               subtractDate(new Date(level.unlocked_at)));
 
         const medianSpeed = median(progressions.slice(0, -1).map(levelDuration).sort( ($0, $1) => $0 - $1));
-        const hypotheticalSpeed = (speed || 240) * 60 * 60;
+        const hypotheticalSpeed = (document.getElementById('speed')?.value || 240) * 60 * 60;
         const hidePast = document.URL.includes('#hidePast');
 
         const expandedLevel = parseInt(document.URL.includes('#L') && document.URL.split('#L')[1]);
 
-        var time = [0];
+        let time = [0];
 
         for (let i = 1; i < stats.length; i++) {
             stats[i].sort( ($0, $1) => $0[0] - $1[0]);
-            var trimmed = stats[i].slice(0, Math.ceil(stats[i].length * 0.9));
+            let trimmed = stats[i].slice(0, Math.ceil(stats[i].length * 0.9));
             time.push(trimmed[trimmed.length - 1][0]);
         }
 
@@ -85,18 +104,16 @@
             }
         }
 
-        const element = document.getElementsByClassName("projections")[0];
-        element.className += " chart";
+        let output = `<label for="speed">Hypothetical Speed (in hours):</label>
+            <input type="number" id="speed" value="240"><span id="buttons"><button id="project">Project</button><br/>
+            <button id="past">Toggle Past Levels</button><button id="current">Show Current Level</button><br/>
+            </span><table class="coverage"><tbody><tr class="header"> ${expandedLevel ?
+            `<td>Kanji</td><td colspan=3>Fastest</td>` :
+        `<td>Level </td><td> Real/Predicted </td><td> Fastest </td><td> Hypothetical</td>`}</tr>`;
 
-        var output = `<label for="speed">Hypothetical Speed (in hours):</label><input type="number" id="speed" value="240">
-        <button onclick="project(document.getElementById('speed').value);">Project</button><br/>
-        <button id="past" onclick="setHidePast();">Toggle Past Levels</button><br/>
-        <button onclick="setCurrent();">Show Current Level</button><br/>
-        <table class="coverage"><tbody><tr class="header"> ${expandedLevel ? `<td>Kanji</td><td colspan=3>Fastest</td>` : `<td>Level </td><td> Real/Predicted </td><td> Fastest </td><td> Hypothetical</td>`}</tr>`;
-
-        var d = new Date(), d1 = null, d2 = null, d3 = null, currentReached = false;
-        for (var level of progressions) {
-            var s = "";
+        let d = new Date(), d1 = null, d2 = null, d3 = null, currentReached = false;
+        for (const level of progressions) {
+            let s = "";
             if (level === current) currentReached = true;
             if (hidePast && !currentReached) continue;
 
@@ -128,38 +145,27 @@
             }
 
             if (!expandedLevel) {
-                output += `<tr ${level === current ? "class='current_level'" : ""}><td> ${String("0" + level.level).slice(-2)} </td> ${s} </tr>`;
+                output += `<tr ${level === current ?
+                    "class='current_level'" : ""}><td> ${String("0" + level.level).slice(-2)} </td> ${s} </tr>`;
             }
         }; // level progressions
 
         output += "</tbody></table>";
+
+        const element = document.getElementsByClassName("projections")[0];
+        if (!element.className.includes("chart")) element.className += " chart";
         element.innerHTML = output;
 
-        addGlobalStyle(`
-.main-content .chart table.coverage {margin-top:1em; position:relative;}
-.main-content .chart table.coverage {border-collapse:collapse; border-spacing:0; margin-left:auto; margin-right:auto;}
-.main-content .chart table.coverage tr {border-left:1px solid #000;}
-.main-content .chart table.coverage tr:first-child {border-top:1px solid #000;}
-.main-content .chart table.coverage tr:last-child {border-bottom:1px solid #000;}
-.main-content .chart table.coverage tr.header {background-color:#ffd; font-weight:bold;}
-.main-content .chart table.coverage tr.header:nth-child(2) {line-height:1em;}
-.main-content .chart table.coverage tr.header.bottom {border-bottom:1px solid #000;}
-.main-content .chart table.coverage tr:not(.header) + tr:not(.header):not(.current_level) {border-top:1px solid #ddd;}
-.main-content .chart table.coverage tr:not(.header):nth-child(even) {background-color:#efe;}
-.main-content .chart table.coverage td {padding:0 .5em;}
-.main-content .chart table.coverage td:first-child {border-right:1px solid #000;}
-.main-content .chart table.coverage td:last-child {border-right:1px solid #000;}
-.main-content .chart table.coverage tr.header td.header_div {border-bottom:1px solid #0001;}
-.main-content .chart table.coverage tr.count td {font-weight:normal; font-size:0.625em;}
-.main-content .chart table.coverage tr.current_level {border:2px solid #000;}
-.main-content .chart table.coverage tr.current_level:after {content:"\f061"; font-family:FontAwesome; position:absolute; display:inline-block; left:-1.25em;}
+        document.getElementById("project").onclick = project;
+        document.getElementById("past").onclick = set(document.URL.includes('#hidePast') ? '#' : '#hidePast');
+        document.getElementById("current").onclick = set('#L' + current.level);
 
-        `);
+        addGlobalStyle();
     }
 
-    window.handleAPI = (userData, levels, systems, items) => {
+    const api = function api(userData, levels, systems, items) {
         maxLevel = userData.subscription.max_level_granted;
-        for (var id in levels) {
+        for (const id in levels) {
             progressions.push(levels[id].data);
         }
         current = progressions[progressions.length - 1];
@@ -172,17 +178,18 @@
             this.setTime(this.getTime() - date.getTime());
             return this.getTime() / 1000;
         }
-        function findID(arr, id) {
+        const findID = function findID(arr, id) {
             return arr.find(o => o.id === id);
-        }
+        };
 
         const date = new Date();
-        var getLength = item => {
+        const getLength = function(item) {
             if (!item.assignments || !item.assignments.passed_at) {
-                var interval = item.assignments && item.assignments.available_at ? Math.max(0, (new Date(item.assignments.available_at)).subtractDate(date)) : 0;
+                let interval = item.assignments && item.assignments.available_at ?
+                    Math.max(0, (new Date(item.assignments.available_at)).subtractDate(date)) : 0;
                 const system = systems[item.data.spaced_repetition_system_id];
-                const passingStage = system.data.passing_stage_position;
-                for (var stage = (item.assignments ? item.assignments.srs_stage : -1) + 1; stage < passingStage; stage++) {
+                const passing = system.data.passing_stage_position;
+                for (let stage = (item.assignments ? item.assignments.srs_stage : -1) + 1; stage < passing; stage++) {
                     interval += system.data.stages[stage].interval;
                 }
                 return interval;
@@ -191,7 +198,7 @@
         };
 
         stats = Array.from(Array(maxLevel + 1), () => []);
-        for (var item of items) {
+        for (const item of items) {
             if (item.data.hidden_at || item.object !== "kanji") continue;
             const level = item.data.level;
             const radicals = item.data.component_subject_ids.map(id => {
@@ -202,7 +209,7 @@
             stats[item.data.level].push([length, item.data.characters]);
         }
 
-        window.project(null);
+        project(null);
     };
 
     window.wkof.ready('ItemData, Apiv2').then(() => {
@@ -210,15 +217,11 @@
             window.wkof.Apiv2.get_endpoint('level_progressions').then(progressions => {
                 window.wkof.Apiv2.get_endpoint('spaced_repetition_systems').then(systems => {
                     window.wkof.ItemData.get_items('subjects, assignments').then(items => {
-                        function entered_progression_page() {
-                            window.handleAPI(userData, progressions, systems, items);
-                        }
-
-                        //Enable callback when we enter the progression page
-                        window.wkof.on('wkstats.projections.loaded', entered_progression_page);
-                        //Manually check if the initial page was the progression page, in which case the callback doesn't fire
-                        if(window.location.href == 'https://www.wkstats.com/progress/projections') {
-                            entered_progression_page();
+                        // Enable callback when we enter the progression page
+                        window.wkof.on('wkstats.projections.loaded', () => api(userData, progressions, systems, items));
+                        // Manually check if the initial page was the progression page, when callback may not fire
+                        if (window.location.href == 'https://www.wkstats.com/progress/projections') {
+                            api(userData, progressions, systems, items);
                         }
                     });
                 });
