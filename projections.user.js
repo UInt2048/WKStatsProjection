23f5cfd4fbae00d7e3c15ef8c75875e699d2ec02
UInt2048 <46784000+UInt2048@users.noreply.github.com>
2022-01-19 21:02:40 -0500

    Extend current level view to any level

--- projections.user.js
+++ projections.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WKStats Projections Page
-// @version      1.1.5
+// @version      1.2.0
 // @description  Make a temporary projections page for WKStats
 // @author       UInt2048
 // @include      https://www.wkstats.com/*
@@ -13,13 +13,13 @@
 /*eslint max-len: ["error", { "code": 120 }]*/
 
 (function() {
-    'use strict';
-    window.wkof.include('ItemData, Apiv2');
+    "use strict";
+    window.wkof.include("ItemData, Apiv2");
 
-    let maxLevel = null, progressions = [], current = null, stats = null;
+    let maxLevel = null, progression = [], current = null, stats = null, now = null;
 
     const addGlobalStyle = function addGlobalStyle() {
-        const head = document.getElementsByTagName('head')[0], css = `
+        const head = document.getElementsByTagName("head")[0], css = `
 .main-content .chart table.coverage {margin-top:1em; position:relative;}
 .main-content .chart table.coverage {border-collapse:collapse; border-spacing:0; margin-left:auto; margin-right:auto;}
 .main-content .chart table.coverage tr {border-left:1px solid #000;}
@@ -40,61 +40,52 @@
 {content:"\f061"; font-family:FontAwesome; position:absolute; display:inline-block; left:-1.25em;}
         `;
         if (head) {
-            const style = document.createElement('style');
-            style.type = 'text/css';
-            style.innerHTML = css.replace(/;/g, ' !important;');
+            const style = document.createElement("style");
+            style.type = "text/css";
+            style.innerHTML = css.replace(/;/g, " !important;");
             head.appendChild(style);
         }
     }
 
-    const set = function set(url) {
+    const set = function set(url, item) {
         return () => {
-            history.pushState({}, null, url);
+            window.history.pushState({}, null, url + (document.getElementById(item)?.value ?? ""));
             project();
         };
     }
 
+    const median = function median(arr) {
+        const mid = Math.floor(arr.length / 2);
+        return arr.length % 2 !== 0 ? arr[mid] : (arr[mid - 1] + arr[mid]) / 2;
+    }
+
+    const levelDuration = function(lvl) {
+        return new Date(lvl.passed_at ? lvl.passed_at : lvl.abandoned_at).subtractDate(new Date(lvl.unlocked_at));
+    }
+
+    Date.prototype.add = function(seconds) {
+        this.setTime(this.getTime() + (seconds*1000));
+        return this;
+    }
+    Date.prototype.subtractDate = function(date) {
+        this.setTime(this.getTime() - date.getTime());
+        return this.getTime() / 1000;
+    }
+    Date.prototype.format = function() {
+        return new window.Intl.DateTimeFormat([], { dateStyle: "medium", timeStyle: "medium" }).format(this);
+    }
+
     const project = function project() {
-        Date.prototype.add = function(seconds) {
-            this.setTime(this.getTime() + (seconds*1000));
-            return this;
-        }
-        Date.prototype.subtractDate = function(date) {
-            this.setTime(this.getTime() - date.getTime());
-            return this.getTime() / 1000;
-        }
-        Date.prototype.format = function() {
-            try {
-                return new Intl.DateTimeFormat([], { dateStyle: 'medium', timeStyle: 'medium' }).format(this);
-            } catch (e) {
-                if (e instanceof RangeError) {
-                    console.log("Project called too soon!");
-                }
-                throw e;
-            }
-        }
-        const median = function median(arr) {
-            const mid = Math.floor(arr.length / 2);
-            return arr.length % 2 !== 0 ? arr[mid] : (arr[mid - 1] + arr[mid]) / 2;
-        }
-
+        const progressions = progression.slice();
         const levels = progressions.map($0 => $0.level);
-
-        const levelDuration = (level => new Date(level.passed_at ? level.passed_at : level.abandoned_at).
-                               subtractDate(new Date(level.unlocked_at)));
-
         const medianSpeed = median(progressions.slice(0, -1).map(levelDuration).sort( ($0, $1) => $0 - $1));
-        const hypotheticalSpeed = (document.getElementById('speed')?.value || 240) * 60 * 60;
-        const hidePast = document.URL.includes('#hidePast');
-
-        const expandedLevel = parseInt(document.URL.includes('#L') && document.URL.split('#L')[1]);
+        const hypotheticalSpeed = (document.getElementById("speed")?.value || 240) * 60 * 60;
+        const hidePast = document.URL.includes("#hidePast");
 
         let time = [0];
 
-        for (let i = 1; i < stats.length; i++) {
-            stats[i].sort( ($0, $1) => $0[0] - $1[0]);
-            let trimmed = stats[i].slice(0, Math.ceil(stats[i].length * 0.9));
-            time.push(trimmed[trimmed.length - 1][0]);
+        for (const data of stats.slice(1)) {
+            time.push(data.sort(($0, $1) => $0[0] - $1[0])[Math.ceil(data.length * 0.9)][0]);
         }
 
         if (levels[levels.length - 1] < maxLevel) {
@@ -104,25 +95,24 @@
             }
         }
 
+        const expanded = parseInt(document.URL.includes("#L") && document.URL.split("#L")[1]);
+        const expandedLevel = expanded && progressions.slice().reverse().find($0 => expanded == $0.level);
+
         let output = `<label for="speed">Hypothetical Speed (in hours):</label>
             <input type="number" id="speed" value="240"><span id="buttons"><button id="project">Project</button><br/>
-            <button id="past">Toggle Past Levels</button><button id="current">Show Current Level</button><br/>
+            <button id="past">Toggle Past Levels</button><br/>
+            <label for="speed">Show Details for Level:</label>
+            <input type="number" id="expanded" value="${current.level}">
+            <button id="detailed">Show Details</button><br/>
             </span><table class="coverage"><tbody><tr class="header"> ${expandedLevel ?
-            `<td>Kanji</td><td colspan=3>Fastest</td>` :
-        `<td>Level </td><td> Real/Predicted </td><td> Fastest </td><td> Hypothetical</td>`}</tr>`;
+            "<td>Kanji</td><td colspan=3>Fastest</td>" :
+        "<td>Level </td><td> Real/Predicted </td><td> Fastest </td><td> Hypothetical</td>"}</tr>`;
 
-        let d = new Date(), d1 = null, d2 = null, d3 = null, currentReached = false;
+        let d = new Date(now), d1 = null, d2 = null, d3 = null, currentReached = false;
         for (const level of progressions) {
             let s = "";
             if (level === current) currentReached = true;
             if (hidePast && !currentReached) continue;
-
-            if (expandedLevel) {
-                for (let item of stats[expandedLevel]) {
-                    output += `<tr><td>${item[1]}</td><td colspan=3>${(new Date(d)).add(item[0]).format()}</tr>`;
-                }
-                break;
-            }
 
             if (level.unlocked_at) {
                 d = new Date(level.unlocked_at);
@@ -144,11 +134,16 @@
                 s += `<td> ${d1.format()} </td><td> ${d2.format()} </td><td> ${d3.format()} </td>`;
             }
 
-            if (!expandedLevel) {
+            if (expandedLevel === level) {
+                for (const kanji of stats[expanded]) {
+                    const date = (kanji[0] < 0 ? "Passed on " : "") + (new Date(d2 || now)).add(kanji[0]).format();
+                    output += `<tr><td>${kanji[1]}</td><td colspan=3>${date}</tr>`;
+                }
+            } else if (!expanded) {
                 output += `<tr ${level === current ?
-                    "class='current_level'" : ""}><td> ${String("0" + level.level).slice(-2)} </td> ${s} </tr>`;
+                    "class=\"current_level\"" : ""}><td> ${String("0" + level.level).slice(-2)} </td> ${s} </tr>`;
             }
-        }; // level progressions
+        }
 
         output += "</tbody></table>";
 
@@ -157,36 +152,25 @@
         element.innerHTML = output;
 
         document.getElementById("project").onclick = project;
-        document.getElementById("past").onclick = set(document.URL.includes('#hidePast') ? '#' : '#hidePast');
-        document.getElementById("current").onclick = set('#L' + current.level);
+        document.getElementById("past").onclick = set(document.URL.includes("#hidePast") ? "#" : "#hidePast");
+        document.getElementById("detailed").onclick = set("#L", "expanded");
 
         addGlobalStyle();
     }
 
     const api = function api(userData, levels, systems, items) {
+        if (progression.length > 0) return project();
+
         maxLevel = userData.subscription.max_level_granted;
         for (const id in levels) {
-            progressions.push(levels[id].data);
+            progression.push(levels[id].data);
         }
-        current = progressions[progressions.length - 1];
-
-        Date.prototype.add = function(seconds) {
-            this.setTime(this.getTime() + (seconds*1000));
-            return this;
-        }
-        Date.prototype.subtractDate = function(date) {
-            this.setTime(this.getTime() - date.getTime());
-            return this.getTime() / 1000;
-        }
-        const findID = function findID(arr, id) {
-            return arr.find(o => o.id === id);
-        };
-
-        const date = new Date();
+        current = progression[progression.length - 1];
+        now = new Date();
         const getLength = function(item) {
             if (!item.assignments || !item.assignments.passed_at) {
                 let interval = item.assignments && item.assignments.available_at ?
-                    Math.max(0, (new Date(item.assignments.available_at)).subtractDate(date)) : 0;
+                    Math.max(0, (new Date(item.assignments.available_at)).subtractDate(now)) : 0;
                 const system = systems[item.data.spaced_repetition_system_id];
                 const passing = system.data.passing_stage_position;
                 for (let stage = (item.assignments ? item.assignments.srs_stage : -1) + 1; stage < passing; stage++) {
@@ -194,7 +178,7 @@
                 }
                 return interval;
             }
-            return 0;
+            return (new Date(item.assignments.passed_at)).subtractDate(now);
         };
 
         stats = Array.from(Array(maxLevel + 1), () => []);
@@ -202,27 +186,23 @@
             if (item.data.hidden_at || item.object !== "kanji") continue;
             const level = item.data.level;
             const radicals = item.data.component_subject_ids.map(id => {
-                const radical = findID(items, id);
+                const radical = items.find(o => o.id === id);
                 return radical.data.level === level ? getLength(radical) : 0;
             });
             const length = radicals.reduce((a, b) => Math.max(a, b)) + getLength(item);
             stats[item.data.level].push([length, item.data.characters]);
         }
 
-        project(null);
+        project();
     };
 
-    window.wkof.ready('ItemData, Apiv2').then(() => {
-        window.wkof.Apiv2.get_endpoint('user').then(userData => {
-            window.wkof.Apiv2.get_endpoint('level_progressions').then(progressions => {
-                window.wkof.Apiv2.get_endpoint('spaced_repetition_systems').then(systems => {
-                    window.wkof.ItemData.get_items('subjects, assignments').then(items => {
+    window.wkof.ready("ItemData, Apiv2").then(() => {
+        window.wkof.Apiv2.get_endpoint("user").then(userData => {
+            window.wkof.Apiv2.get_endpoint("level_progressions").then(progressions => {
+                window.wkof.Apiv2.get_endpoint("spaced_repetition_systems").then(systems => {
+                    window.wkof.ItemData.get_items("subjects, assignments").then(items => {
                         // Enable callback when we enter the progression page
-                        window.wkof.on('wkstats.projections.loaded', () => api(userData, progressions, systems, items));
-                        // Manually check if the initial page was the progression page, when callback may not fire
-                        if (window.location.href == 'https://www.wkstats.com/progress/projections') {
-                            api(userData, progressions, systems, items);
-                        }
+                        window.wkof.on("wkstats.projections.loaded", () => api(userData, progressions, systems, items));
                     });
                 });
             });
